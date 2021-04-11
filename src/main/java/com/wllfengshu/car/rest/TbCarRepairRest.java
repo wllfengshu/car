073ebda2904e58ff91e42eefa69922ac1e9e0a55
wllfengshu <wllfengshu@163.com>
2021-04-11 20:03:15 +0800

    修车和洗车查询不需要时间条件

--- src/main/java/com/wllfengshu/car/rest/TbCarRepairRest.java
+++ src/main/java/com/wllfengshu/car/rest/TbCarRepairRest.java
@@ -92,12 +92,11 @@ public class TbCarRepairRest {
 
     @ApiOperation(value = "查询列表", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "beginTime", value = "开始时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
-            @ApiImplicitParam(name = "endTime", value = "结束时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
+//            @ApiImplicitParam(name = "beginTime", value = "开始时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
+//            @ApiImplicitParam(name = "endTime", value = "结束时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
             @ApiImplicitParam(name = "customerName", value = "客户姓名", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "customerNickname", value = "客户昵称", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "phone", value = "电话", dataType = "string", paramType = "query"),
-            @ApiImplicitParam(name = "otherContacts", value = "其他联系人", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "licensePlate", value = "车牌", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "pageNo", value = "页数(从0开始，默认0)", example = "0", dataType = "int", paramType = "query"),
             @ApiImplicitParam(name = "pageSize", value = "每页的数量(默认10)", example = "10", dataType = "int", paramType = "query"),
@@ -108,12 +107,11 @@ public class TbCarRepairRest {
     })
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Map<String, Object> selects(
-            @RequestParam(value = "beginTime") String beginTime,
-            @RequestParam(value = "endTime") String endTime,
+//            @RequestParam(value = "beginTime") String beginTime,
+//            @RequestParam(value = "endTime") String endTime,
             @RequestParam(value = "customerName", required = false) String customerName,
             @RequestParam(value = "customerNickname", required = false) String customerNickname,
             @RequestParam(value = "phone", required = false) String phone,
-            @RequestParam(value = "otherContacts", required = false) String otherContacts,
             @RequestParam(value = "licensePlate", required = false) String licensePlate,
             @RequestParam(value = "pageNo", required = false, defaultValue = "0") Integer pageNo,
             @RequestParam(value = "pageSize", required = false, defaultValue = "10") Integer pageSize,
@@ -121,8 +119,8 @@ public class TbCarRepairRest {
             HttpServletRequest request,
             HttpServletResponse response) throws CustomException {
         Map<String, Object> params = new HashMap<>();
-        params.put("beginTime", beginTime);
-        params.put("endTime", endTime);
+//        params.put("beginTime", beginTime);
+//        params.put("endTime", endTime);
         if (StringUtils.isNoneEmpty(customerName)) {
             params.put("customerName", "%" + customerName + "%");
         }
@@ -131,9 +129,6 @@ public class TbCarRepairRest {
         }
         if (StringUtils.isNoneEmpty(phone)) {
             params.put("phone", "%" + phone + "%");
-        }
-        if (StringUtils.isNoneEmpty(otherContacts)) {
-            params.put("otherContacts", "%" + otherContacts + "%");
         }
         if (StringUtils.isNoneEmpty(licensePlate)) {
             params.put("licensePlate", "%" + licensePlate + "%");
--- src/main/java/com/wllfengshu/car/rest/TbCarWashRest.java
+++ src/main/java/com/wllfengshu/car/rest/TbCarWashRest.java
@@ -93,12 +93,11 @@ public class TbCarWashRest {
 
     @ApiOperation(value = "查询列表", httpMethod = "GET")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "beginTime", value = "开始时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
-            @ApiImplicitParam(name = "endTime", value = "结束时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
+//            @ApiImplicitParam(name = "beginTime", value = "开始时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
+//            @ApiImplicitParam(name = "endTime", value = "结束时间(格式:yyyy-MM-dd HH:mm:ss)", required = true, dataType = "date", paramType = "query"),
             @ApiImplicitParam(name = "customerName", value = "客户姓名", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "customerNickname", value = "客户昵称", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "phone", value = "电话", dataType = "string", paramType = "query"),
-            @ApiImplicitParam(name = "otherContacts", value = "其他联系人", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "licensePlate", value = "车牌", dataType = "string", paramType = "query"),
             @ApiImplicitParam(name = "pageNo", value = "页数(从0开始，默认0)", example = "0", dataType = "int", paramType = "query"),
             @ApiImplicitParam(name = "pageSize", value = "每页的数量(默认10)", example = "10", dataType = "int", paramType = "query"),
@@ -109,12 +108,11 @@ public class TbCarWashRest {
     })
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Map<String, Object> selects(
-            @RequestParam(value = "beginTime") String beginTime,
-            @RequestParam(value = "endTime") String endTime,
+//            @RequestParam(value = "beginTime") String beginTime,
+//            @RequestParam(value = "endTime") String endTime,
             @RequestParam(value = "customerName", required = false) String customerName,
             @RequestParam(value = "customerNickname", required = false) String customerNickname,
             @RequestParam(value = "phone", required = false) String phone,
-            @RequestParam(value = "otherContacts", required = false) String otherContacts,
             @RequestParam(value = "licensePlate", required = false) String licensePlate,
             @RequestParam(value = "pageNo", required = false, defaultValue = "0") Integer pageNo,
             @RequestParam(value = "pageSize", required = false, defaultValue = "10") Integer pageSize,
@@ -122,8 +120,8 @@ public class TbCarWashRest {
             HttpServletRequest request,
             HttpServletResponse response) throws CustomException {
         Map<String, Object> params = new HashMap<>();
-        params.put("beginTime", beginTime);
-        params.put("endTime", endTime);
+//        params.put("beginTime", beginTime);
+//        params.put("endTime", endTime);
         if (StringUtils.isNoneEmpty(customerName)) {
             params.put("customerName", "%" + customerName + "%");
         }
@@ -132,9 +130,6 @@ public class TbCarWashRest {
         }
         if (StringUtils.isNoneEmpty(phone)) {
             params.put("phone", "%" + phone + "%");
-        }
-        if (StringUtils.isNoneEmpty(otherContacts)) {
-            params.put("otherContacts", "%" + otherContacts + "%");
         }
         if (StringUtils.isNoneEmpty(licensePlate)) {
             params.put("licensePlate", "%" + licensePlate + "%");
